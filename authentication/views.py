f3df7a947c4c17e5725d35483bc6af3501f330c2
ibn-A <ibn_ali@yahoo.com>
2023-04-26 11:34:05 +0200

    Rectification import ligne 1 on views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-qfrom django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 from . import forms
